10061e16454b15cf7365496e83ef4ef3900fddfd
win-will <105662845+win-will@users.noreply.github.com>
2023-01-05 21:35:26 -0500

    Revert "Styling login"

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -33,14 +33,14 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/profile');
+    window.location.assign('/');
   }
 
   logout() {
     // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
     // this will reload the page and reset the state of the application
-    window.location.assign('/');
+    window.location.reload();
   }
 }
 
