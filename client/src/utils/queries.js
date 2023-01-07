1ce0a8dbaa6368325ce046163bcb0d66c6e64973
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2023-01-07 16:00:37 -0700

    change to queries.js, udating query to GET for classes and single class

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -17,34 +17,38 @@ query user($username: String!) {
 
 //Qeuries based on activity 26 in lesson 21 MERN
 //Query Classes (Based on typeDefs)
-export const QUERY_CLASSES = gql`
-  query classes {
-    classes {
-      _id
-      name
-      leadId
-      description
-      userIds
-      teamIds
-      inviteIds
-      classAvatarId
-    }
+export const GET_CLASSES = gql`
+query Query {
+  allClasses {
+    userIds
+    teamSize
+    teamIds
+    name
+    leadId
+    inviteIds
+    classAvatarId
+    classSize
+    description
+    _id
   }
+}
 `;
 //Query Class (Based on typeDefs)
-export const QUERY_SINGLE_CLASS = gql`
-  query class($classId: ID!) {
-    class(classId: $classId) {
-      _id
-      name
-      leadId
-      description
-      userIds
-      teamIds
-      inviteIds
-      classAvatarId
-    }
+export const GET_SINGLE_CLASS = gql`
+query Query($classId: ID!) {
+  singleClass(classId: $classId) {
+    name
+    userIds
+    teamSize
+    teamIds
+    leadId
+    inviteIds
+    description
+    classSize
+    classAvatarId
+    _id
   }
+}
 `;
 //Query Teams (Based on typeDefs)
 export const QUERY_TEAMS = gql`
