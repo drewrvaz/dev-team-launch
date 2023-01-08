46ab811e16339063b84a7c64472b2cf877aec12c
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2023-01-07 20:57:18 -0700

    deploy prep

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/devteamlaunchdb', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/devteamlaunchdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
