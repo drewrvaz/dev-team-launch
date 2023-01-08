1d6f6fd30d66eaacba6edc0fbb05acee10814d98
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2023-01-08 16:30:44 -0700

    deploy prep

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const env = require('dotenv'.config());
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mongo:c1brL9G7CtbJQAiw@cluster0.xu7enbg.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
