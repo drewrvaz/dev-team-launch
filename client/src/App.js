f9ffab6bdc6ca84b8878e5628fab08ea9ab5b8e8
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2023-01-08 21:18:35 -0700

    update uri to /graphql

--- client/src/App.js
+++ client/src/App.js
@@ -52,7 +52,7 @@ import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
